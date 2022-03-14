c200ff69c3142197d59e642599169cac09e3a331
abubasith456 <abubasith456@gmail.com>
2022-03-14 20:02:07 +0530

    news adapter and category adapter inserted but some errors remain

---
+++ app/src/main/java/com/example/kotlinlearning/model/Category.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinlearning.model
+
+class Category {
+
+    val selectCategory = arrayOf(
+        "sports",
+        "business",
+        "entertainment",
+        "health",
+        "science",
+        "technology",
+        "general"
+    )
+
+    val selectSettings = arrayOf(
+        "Offline news",
+        "Logout"
+    )
+}
--- app/src/main/java/com/example/kotlinlearning/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/example/kotlinlearning/viewModel/NewsViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinlearning.viewModel
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
@@ -8,7 +10,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kotlinlearning.adapter.NewsAdapter
 import com.example.kotlinlearning.databinding.NewsFragmentBinding
+import com.example.kotlinlearning.fragment.NewsFragment
 import com.example.kotlinlearning.model.Article
+import com.example.kotlinlearning.model.Category
 import com.example.kotlinlearning.model.NewsResponse
 import com.example.kotlinlearning.rest_api.BCRequests
 import retrofit2.Call
@@ -26,20 +30,99 @@ class NewsViewModel : ViewModel() {
     private val newsHeadlines: MutableLiveData<List<Article>> =
         MutableLiveData<List<Article>>()
 
+    private val newsCategoryHeadlines: MutableLiveData<List<Article>> =
+        MutableLiveData<List<Article>>()
+
 
     lateinit var activity: Activity
+    lateinit var newsFragment: Class<NewsFragment>
 
     fun getActivity(activity: Activity) {
         this.activity = activity
     }
 
-//    init {
-//        loadNews()
-//    }
+    fun onCategoryClick() {
+        try {
+            val category = Category()
+            val builder = AlertDialog.Builder(activity)
+            builder.setTitle("Select Category")
+                .setItems(
+                    category.selectCategory,
+                    DialogInterface.OnClickListener { dialog, which ->
+                        val selectedCategory: String = category.selectCategory.get(which)
+                        Toast.makeText(
+                            activity,
+                            "" + selectedCategory,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        loadFilterItems(selectedCategory)
+                    }).show()
 
-    //    public void getAdapterPosition(List<NewsHeadLines> newsHeadLines) {
-    //        Toast.makeText(application.getApplicationContext(), "" + newsHeadLines.get(0), Toast.LENGTH_SHORT).show();
-    //    }
+        } catch (e: Exception) {
+            Log.e("Api call error==> ", e.message.toString())
+        }
+    }
+
+    private fun loadFilterItems(selectedCategory: String) {
+
+        try {
+            val call: Call<NewsResponse> =
+                BCRequests().bCRestService.getNewsData(country, selectedCategory, null, api)
+            call.enqueue(object : Callback<NewsResponse> {
+                override fun onResponse(
+                    call: Call<NewsResponse>,
+                    response: Response<NewsResponse>
+                ) {
+                    if (response.isSuccessful()) {
+                        Toast.makeText(
+                            activity,
+                            "Success",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val responseBody = response.body()?.articles
+//                        newsAdapter = responseBody?.let { NewsAdapter(activity, it) }!!
+//                        newsAdapter.notifyDataSetChanged()
+//                        binding.recyclerView.adapter = newsAdapter
+                        newsCategoryHeadlines.value = responseBody!!
+                    }
+                }
+
+                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                    Toast.makeText(
+                        activity,
+                        t.message,
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    Log.e("Result==> ", t.message.toString())
+                }
+            })
+
+        } catch (e: Exception) {
+            Log.e("Api call error==> ", e.message.toString())
+        }
+
+    }
+
+    fun onSettingClick() {
+        val category = Category()
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Settings").setItems(
+            category.selectSettings,
+            DialogInterface.OnClickListener { dialogInterface, which ->
+                val selectedCategory: String = category.selectSettings.get(which)
+                Toast.makeText(
+                    activity,
+                    "" + selectedCategory,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }).show()
+    }
+
+    fun getCategoryHeadlines(): LiveData<List<Article>> {
+        return newsCategoryHeadlines
+    }
+
     fun getNewsHeadlines(): LiveData<List<Article>> {
         try {
             val call: Call<NewsResponse> =
@@ -50,7 +133,11 @@ class NewsViewModel : ViewModel() {
                     response: Response<NewsResponse>
                 ) {
                     if (response.isSuccessful()) {
-                        Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            activity,
+                            "Success",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         val responseBody = response.body()?.articles
                         val responseTotal = response.body()?.totalResults
 
@@ -58,15 +145,15 @@ class NewsViewModel : ViewModel() {
 //                        newsAdapter.notifyDataSetChanged()
 //                        binding.recyclerView.adapter = newsAdapter
                         newsHeadlines.value = responseBody!!
-//                        for (myData in responseBody?.toList()!!) {
-//                            Log.e("Result==> ", myData.title)
-//                        }
-//                        Log.e("Result==> ", responseTotal.toString())
                     }
                 }
 
                 override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                    Toast.makeText(activity, t.message, Toast.LENGTH_SHORT)
+                    Toast.makeText(
+                        activity,
+                        t.message,
+                        Toast.LENGTH_SHORT
+                    )
                         .show()
                     Log.e("Result==> ", t.message.toString())
                 }
@@ -76,43 +163,6 @@ class NewsViewModel : ViewModel() {
             Log.e("Api call error==> ", e.message.toString())
         }
         return newsHeadlines
-    }
-
-    fun loadNews() {
-        try {
-            val call: Call<NewsResponse> =
-                BCRequests().bCRestService.getNewsData(country, category, null, api)
-            call.enqueue(object : Callback<NewsResponse> {
-                override fun onResponse(
-                    call: Call<NewsResponse>,
-                    response: Response<NewsResponse>
-                ) {
-                    if (response.isSuccessful()) {
-                        Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show()
-                        val responseBody = response.body()?.articles
-                        val responseTotal = response.body()?.totalResults
-
-                        newsAdapter = responseBody?.let { NewsAdapter(activity, it) }!!
-                        newsAdapter.notifyDataSetChanged()
-                        binding.recyclerView.adapter = newsAdapter
-
-                        for (myData in responseBody?.toList()!!) {
-                            Log.e("Result==> ", myData.title)
-                        }
-                        Log.e("Result==> ", responseTotal.toString())
-                    }
-                }
-
-                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                    Toast.makeText(activity, t.message, Toast.LENGTH_SHORT)
-                        .show()
-                    Log.e("Result==> ", t.message.toString())
-                }
-            })
-
-        } catch (e: Exception) {
-            Log.e("Api call error==> ", e.message.toString())
-        }
     }
 
 
